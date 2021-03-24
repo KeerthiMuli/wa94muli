aa68297171ed7405e664916e7011671d707b277c
KeerthiMuli <77706824+KeerthiMuli@users.noreply.github.com>
2021-03-24 14:15:43 -0500

    User access has been added to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var muli = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  muli = muli+1;
+  res.send('User accesses are: ' + muli);
 });
 
 module.exports = router;
